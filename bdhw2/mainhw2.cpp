c6129f746110329ec031182737aa84714e82b55a
Juan Pablo Alonso <jalonso@xubuntu.(none)>
2013-03-13 18:44:01 -0400

    cleaned new functor

--- bdhw2/mainhw2.cpp
+++ bdhw2/mainhw2.cpp
@@ -11,6 +11,7 @@
 #include <thrust/transform.h>
 #include <thrust/reduce.h>
 #include <thrust/host_vector.h>
+#include <thrust/iterator/counting_iterator.h>
 //#include <thrust/copy.h>
 //#include <thrust/fill.h>
 //#include <thrust/replace.h>
@@ -73,24 +74,24 @@ struct get_CVA : public thrust::unary_function<unsigned int,float>
   float operator()(unsigned int thread_id)
   {
     float sum = 0;
-    unsigned int N = NUM_SIMULATIONS; // samples per thread
+    unsigned int N = 100; // samples per thread
 
-    unsigned int seed = thread_id;
+//    unsigned int seed = thread_id;
 
     // seed a random number generator
-    thrust::default_random_engine rng(seed);
+//    thrust::default_random_engine rng(seed);
 
     // create a mapping from random numbers to [0,1)
-    thrust::uniform_real_distribution<float> u01(0,1);
+  //  thrust::uniform_real_distribution<float> u01(0,1);
 
-    float timeStep=YEARS/float(NUM_TIMESTEPS);
-    float time=0;
-    float defProb=0;
+    //float timeStep=YEARS/float(NUM_TIMESTEPS);
+    //float time=0;
+    //float defProb=0;
 
     // take N samples
     for(unsigned int i = 0; i < N; ++i)
     {
-    	time=time+timeStep;
+    //	time=time+timeStep;
     	defProb=0.0001;
     	sum+=defProb;
     }
@@ -111,21 +112,7 @@ double genPaths(float _factor,vector<counterParties>& _cp)
 	                                            thrust::plus<float>());
 	return estimate/NUM_SIMULATIONS;
 }
-//
-//double genPaths2(long _steps,float _initial,float _factor,vector<counterParties>& cp)
-//{
-//	thrust::host_vector<float> hX(PARTIES_NUM);
-//    thrust::sequence(hX.begin(), hX.end());
-//	thrust::device_vector<float> X=hX;
-//	thrust::device_vector<counterParties> dcp(cp.begin(),cp.end());
-//
-//	counterParties *raw_ptr = thrust::raw_pointer_cast(&dcp[0]);
-//
-//	double average=0;
-//    thrust::plus<double> binary_op;
-//    return thrust::transform_reduce(X.begin(), X.end(),path_generation(_steps,_initial,_factor,raw_ptr),average,binary_op);
-//    return 1;
-//}
+
 
 int main(){
 	cout<<"starting..."<<float(clock()) / float(CLOCKS_PER_SEC)<<endl;
