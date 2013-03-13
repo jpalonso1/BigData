0a54b27524c3b395dfa0cb8194d84833b44d27fb
Juan Pablo Alonso <jalonso@xubuntu.(none)>
2013-03-13 17:24:27 -0400

    fixed device copy, restored functor

--- bdhw2/parameters.h
+++ bdhw2/parameters.h
@@ -16,7 +16,7 @@ const float STARTING_PRICE = 1.4;
 const float VARIANCE=0.2;
 const float DISCOUNT=0.06;
 const long NUM_TIMESTEPS = 1000;
-const long NUM_SIMULATIONS = 120;
+const long NUM_SIMULATIONS = 100;
 const float YEARS = 5;
 const int PROP_CUTOFF[5] = { 1, 3, 7, 15, 31 };
 
