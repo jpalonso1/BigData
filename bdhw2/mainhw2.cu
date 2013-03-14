0b48d905cdd1748d0018c607581831b588997ac6
Juan Pablo Alonso <jalonso@xubuntu.(none)>
2013-03-13 21:14:36 -0400

    removed old struct

--- bdhw2/mainhw2.cu
+++ bdhw2/mainhw2.cu
@@ -19,55 +19,6 @@
 
 using namespace std;
 
-//struct path_generation
-//{
-//	const int steps;
-//	const double initial;
-//	const double factor;
-//	const counterParties* cp;
-//
-//    path_generation(int _steps,double _initial,double _factor,counterParties *_cp)
-//    : steps(_steps),initial(_initial),factor(_factor),cp(_cp) {}
-//
-//    __host__ __device__
-//	double operator()(const double& x) const {
-//    	double CVA=0;
-////		double current=initial;
-////		float normal=0;
-////		//check for seed number?
-////		//move outside struct to avoid duplicates?
-////		thrust::random::minstd_rand rng(x);
-////		//thrust::random::experimental::normal_distribution<float> dist(2.0, 3.5);
-////		thrust::uniform_real_distribution<float> dist(0,1.0);
-////		double PI=3.14159265359;
-////		double timeStep=YEARS/double(NUM_TIMESTEPS);
-////		double time=0;
-////		double tempCVA=0;
-////		for (int i=1;i<steps;i++)
-////		{
-////			time+=timeStep;
-////			//use box-muller to get a normal (Thrust normal is not working)
-////			normal=(1/sqrt(2.0*dist(rng)))*cos(2*PI*dist(rng));
-////			//evolve the price path one step
-////			current=current+current*normal*factor;
-////			tempCVA=0;
-////			for (int j=0;j<PARTIES_NUM;j++)
-////			{
-//////				CVA+=((1.0/exp(cp[j].hazardRate*time))-(1.0/exp(cp[j].hazardRate*(time-timeStep))))*
-//////						cp[j].netDeal*current;
-//////				CVA+=cp[j].netDeal/PARTIES_NUM;
-//////				double exposure=current;
-//////				defCurr=exp(-cp[i].hazardRate*time);
-//////				cp[i].CVAVal+=(defCurr-defLast)*exposure*exp(-time*DISCOUNT);
-//////				CVA+=exp(cp[j])
-////			}
-////			CVA+=tempCVA*(1.0/exp(time*DISCOUNT));
-////		}
-////		cout<<CVA<<endl;
-//		return CVA;
-//    }
-//};
-
 struct get_CVA : public thrust::unary_function<unsigned int,float>
 {
 	//hazard rate
