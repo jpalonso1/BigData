c025b8f6e1f2d7d4d1a1e0f66741128866cb3425
jpalonso1 <jalonso@xubuntu.(none)>
2013-04-27 02:47:43 -0400

    hw3 noise output working

--- bdhw3/mainhw3.cpp
+++ bdhw3/mainhw3.cpp
@@ -1,4 +1,7 @@
+//Juan Pablo Alonso
+
 #include <cstdlib>
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <xlog.h>
@@ -14,6 +17,9 @@ using namespace bson;
 using namespace mongo;
 
 bool checkDuplicates(string& target,string* compVector){
+	//DES: compare a target string to each string in compVector
+	//IN: target string, array of strings
+	//OUT: true if duplicate found, false otherwise
 	for (int i=0;i<DUPLICATE_ARRAY_SIZE;i++){
 		if (compVector[i].compare(target) == 0){
 			return true;
@@ -23,6 +29,9 @@ bool checkDuplicates(string& target,string* compVector){
 }
 
 void parseFile(ifstream& input,DBClientConnection& c){
+	//DES: parse text file and input into mongo database
+	//IN: input text file, mongo db connection
+
 	//clean up database if it already exists
 	c.dropDatabase("hw3");
 	//hold parsed elements
@@ -39,10 +48,9 @@ void parseFile(ifstream& input,DBClientConnection& c){
 	int compCount=0;
 	bool duplicate;
 	int test=0;
-	XLog logParse("Parsing...");
+	XLog logInsert("Inserting...");
 
-	while(!input.eof())
-	{
+	while(!input.eof()){
 		duplicate=false;
 		//parse a line
 		getline(input,date,',');
@@ -58,11 +66,11 @@ void parseFile(ifstream& input,DBClientConnection& c){
 		b.append("volume", iVol);
 
 		//check for duplicates within a window of size DUPLICATE_ARRAY_SIZE
-		//		if (total>DUPLICATE_ARRAY_SIZE)
 		duplicate=checkDuplicates(date,comparison);
 		comparison[compCount]=date;
 		compCount++;
 		if (compCount>=DUPLICATE_ARRAY_SIZE)compCount=0;
+
 
 		//create "document" to be inserted into database
 		BSONObj p = b.obj();
@@ -80,40 +88,69 @@ void parseFile(ifstream& input,DBClientConnection& c){
 
 		//insert batch of documents into collection once vector is full
 		if (total%BATCH_SIZE==0){
-			c.insert("hw3.raw", bobjp);
+			c.insert("hw3.signal", bobjp);
 			count=0;
-			if(total%(BATCH_SIZE*10)==0)logParse("Lines inserted so far: ",total);
+			if(total%(BATCH_SIZE*10)==0)logInsert.log("Lines inserted so far: ",total);
 		}
 	}
 	//process leftover elements after eof
 	for (int i=0;i<count;i++){
-		c.insert("hw3.raw", bobjp[i]);
+		c.insert("hw3.signal", bobjp[i]);
 	}
-	logParse.end();
-	cout<<"DUPLICATES "<<test<<endl;
+}
+
+void outputToFile(DBClientConnection& c){
+	ofstream signal;
+	ofstream noise;
+	signal.open("signal.txt");
+	noise.open("noise.txt");
+//	20080803:10:00:00.591824,0.025407,27414383
+//	20080803:10:00:00.961882,0.025606,49029074
+
+	auto_ptr<DBClientCursor> noiseCursor =c.query("hw3.noise", Query());
+	//output to file
+	cout<<"starting...";
+	while (noiseCursor->more()){
+
+		BSONObj single = noiseCursor->next();
+		noise<<single.getStringField("date")<<","<<single.getField("value").Double()
+				<<","<<single.getIntField("volume")<<'\n';
+	}
+
 }
 
 void scrub(DBClientConnection& c){
+	auto_ptr<DBClientCursor> noiseCursor[10];
 	//look for negative volume
-	auto_ptr<DBClientCursor> negVol = c.query("hw3.raw", BSON("volume"<<LTE<<0.0));
-	auto_ptr<DBClientCursor> cursor = c.query("hw3.noise", Query("{volume: {'$lt':0}}"));
-//	auto_ptr<DBClientCursor> cursor = c.query("hw3.raw", Query("{\"date\": \"test\",\"$options\":\"i\"}"));
-	while (negVol->more()){
-		BSONObj singleNoise = negVol->next();
-		c.insert("hw3.noise", singleNoise);
-//		cout<<singleNoise<<endl;
-//		c.remove("hw3.noise",singleNoise);
-//		cout << cursor->next().toString() << endl;
+	noiseCursor[0] = c.query("hw3.signal", BSON("volume"<<LTE<<0.0));
+	//find excessive price (>5 dollars)
+	noiseCursor[1] = c.query("hw3.signal", BSON("value"<<GTE<<5.0));
+	//find excessive negative price (<-5 dollars)
+	noiseCursor[2] = c.query("hw3.signal", BSON("value"<<LTE<<-5.0));
+	//find weekend dates
+	noiseCursor[3] = c.query("hw3.signal", Query("{date: /[0-9]{7}2./i }"));
+	//find 9 am trades
+	noiseCursor[4] = c.query("hw3.signal", Query("{date: /[0-9]{8}:09./i }"));
+	//find 5 pm trades
+	noiseCursor[5] = c.query("hw3.signal", Query("{date: /[0-9]{8}:17./i }"));
+
+//	noiseCursor[4] = c.query("hw3.signal", Query("{date: /[0-9]{8}:17./i }"));
+	for (int i=0;i<6;i++){
+		while (noiseCursor[i]->more()){
+			BSONObj singleNoise = noiseCursor[i]->next();
+			//add to noise db
+			c.insert("hw3.noise", singleNoise);
+			//remove from signal db
+			c.remove("hw3.signal",singleNoise);
+	//		cout << cursor->next().toString() << endl;
+		}
+//		cout<<i<<" factor: "<<noiseCursor[i]->itcount()<<endl;
 	}
-	cout<<"pos vol count:"<<cursor->itcount()<<endl;
-	cout<<"neg vol count:"<<negVol->itcount()<<endl;
-	cout<<"scrub inside end"<<endl;
 }
 
 int main(int argc,char* argv[]){
 	//check for file name
-	if (argc<2)
-	{
+	if (argc<2){
 		cout<<"NO FILENAME PROVIDED";
 		return 0;
 	}
@@ -123,19 +160,18 @@ int main(int argc,char* argv[]){
 	//connect to the mongo db localhost
 	DBClientConnection c;
 	c.connect("localhost");
+
+	XLog logParse("Parsing...");
 //	parseFile(input,c);
+	logParse.end();
 
 	XLog logScrub("Scrubbing...");
-	XLog logDupe("Duplicates");
-
-	logDupe.end();
-	scrub(c);
+//	scrub(c);
 	logScrub.end();
 
-	ifstream signal;
-	ifstream noise;
-	signal.open("signal.txt");
-	noise.open("noise.txt");
+	XLog logOutput("Output to file...");
+	outputToFile(c);
+	logOutput.end();
 
-  return EXIT_SUCCESS;
+	return 0;
 }
